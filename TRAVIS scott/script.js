5240df2dc00a8bfa3601f7fba3d1abc1437e499e
deku <bastiangarcia10@hotmail.com>
2025-06-18 14:48:41 -0300

    River

---
+++ TRAVIS scott/script.js
@@ -0,0 +1,44 @@
+
+alert("esta pagina te da informacion de un cantante y puedes descargar el juego con el que colaboro")
+
+
+confirm("si no te gusta entonces andate que nadie te quiere")
+
+
+prompt("te gusta la milanesa con papa fritas")
+
+
+const boton = document.getElementByid('F1');
+
+boton.addEventListener('clik'),  () =>[
+    
+]
+    window.location.href =  'https://www.epicgames.com/site/es-ES/home';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
